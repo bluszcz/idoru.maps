3bc05df9e0cc49a23fdf8876df5415393423f613
Rafał Zawadzki <bluszcz@bluszcz.net>
2010-07-04 01:42:46 +0200

    changed way of generation url for js

--- idoru/maps/interfaces/__init__.py
+++ idoru/maps/interfaces/__init__.py
@@ -1,2 +1,9 @@
 # -*- extra stuff goes here -*-
+from plone.theme.interfaces import IDefaultPloneLayer
+
+class IPlaceKMLSpecific(IDefaultPloneLayer):
+    """
+    Marker interface that defines a Zope 3 skin layer for this product.
+    """
+    pass
 from place import IPlace
