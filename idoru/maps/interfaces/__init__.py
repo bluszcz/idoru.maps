4fc06f7c9069aa51824bc6bec4a532a01ffaa807
Rafał Zawadzki <bluszcz@bluszcz.net>
2010-07-04 01:50:33 +0200

    Removed some orphaned code

--- idoru/maps/interfaces/__init__.py
+++ idoru/maps/interfaces/__init__.py
@@ -1,9 +1,2 @@
 # -*- extra stuff goes here -*-
-from plone.theme.interfaces import IDefaultPloneLayer
-
-class IPlaceKMLSpecific(IDefaultPloneLayer):
-    """
-    Marker interface that defines a Zope 3 skin layer for this product.
-    """
-    pass
 from place import IPlace
